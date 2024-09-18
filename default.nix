ae457909778216870a7ea761be7a28e4899f2add
raf <rraf@tuta.io>
2024-09-19 00:46:14 +0100

    chore: added steam stuff

--- default.nix
+++ default.nix
@@ -1,5 +1,10 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
+  services.udev.packages = [ pkgs.game-devices-udev-rules ];
 
+  programs.steam = {
+    enable = true;
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
+  };
 }
