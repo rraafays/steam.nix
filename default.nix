f68e62ff5f1ecf45f61e7a146d6da626fb6f3162
raf <rraf@tuta.io>
2024-09-22 17:40:31 +0100

    fix: make games run through steam use windows sdl driver

--- default.nix
+++ default.nix
@@ -3,6 +3,11 @@
 {
   programs.steam = {
     enable = true;
+    package = pkgs.steam.override {
+      extraEnv = {
+        SDL_VIDEODRIVER = "windows";
+      };
+    };
     extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
   services.udev.packages = [ pkgs.game-devices-udev-rules ];
