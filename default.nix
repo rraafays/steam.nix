3f72de1ac1ae3461f75fe0e27a8df94525aff7b5
raf <rraf@tuta.io>
2024-09-22 19:18:46 +0100

    update: migrated envrionment variables and cut out some irrelevant ones

--- default.nix
+++ default.nix
@@ -1,14 +1,31 @@
 { pkgs, ... }:
 
 {
+  services.udev.packages = [ pkgs.game-devices-udev-rules ];
   programs.steam = {
     enable = true;
+    extraCompatPackages = [ pkgs.proton-ge-bin ];
     package = pkgs.steam.override {
       extraEnv = {
         SDL_VIDEODRIVER = "windows";
+        WINEDLLOVERRIDES = "dinput8=n,b";
+        ENABLE_VKBASALT = 0;
+        PROTON_HIDE_NVIDIA_GPU = 0;
+        PROTON_ENABLE_NVAPI = 1;
+        PROTON_ENABLE_NGX_UPDATER = 1;
+        PROTON_USE_D9VK = 1;
+        PROTON_USE_VKD3D = 1;
+        DXVK_ASYNC = 1;
+        __GL_VRR_ALLOWED = 1;
+        PROTON_NO_ESYNC = 1;
       };
     };
-    extraCompatPackages = [ pkgs.proton-ge-bin ];
   };
-  services.udev.packages = [ pkgs.game-devices-udev-rules ];
+
+  environment = {
+    systemPackages = [
+      pkgs.mangohud
+      pkgs.gamescope
+    ];
+  };
 }
