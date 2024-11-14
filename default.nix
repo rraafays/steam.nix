e3920e600f500d8a644bd2d8cd9e783a0f2b53df
raf <rraf@tuta.io>
2024-11-14 11:52:49 +0000

    fix: temporarily disabled nvidia related config whilst i dont have an egpu

--- default.nix
+++ default.nix
@@ -6,18 +6,18 @@
     enable = true;
     extraCompatPackages = [ pkgs.proton-ge-bin ];
     package = pkgs.steam.override {
-      extraEnv = {
-        SDL_VIDEODRIVER = "windows";
-        ENABLE_VKBASALT = 0;
-        PROTON_HIDE_NVIDIA_GPU = 0;
-        PROTON_ENABLE_NVAPI = 1;
-        PROTON_ENABLE_NGX_UPDATER = 1;
-        PROTON_USE_D9VK = 1;
-        PROTON_USE_VKD3D = 1;
-        DXVK_ASYNC = 1;
-        __GL_VRR_ALLOWED = 1;
-        PROTON_NO_ESYNC = 1;
-      };
+      # extraEnv = {
+      #   SDL_VIDEODRIVER = "windows";
+      #   ENABLE_VKBASALT = 0;
+      #   PROTON_HIDE_NVIDIA_GPU = 0;
+      #   PROTON_ENABLE_NVAPI = 1;
+      #   PROTON_ENABLE_NGX_UPDATER = 1;
+      #   PROTON_USE_D9VK = 1;
+      #   PROTON_USE_VKD3D = 1;
+      #   DXVK_ASYNC = 1;
+      #   __GL_VRR_ALLOWED = 1;
+      #   PROTON_NO_ESYNC = 1;
+      # };
     };
   };
 
